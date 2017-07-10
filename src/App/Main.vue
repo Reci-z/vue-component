7e1cf275752e86de5194fa39c47b221292e5b995
zhuwangjie <zhuwangjie@jishu.local>
2017-07-10 22:52:34 +0800

    first

---
+++ src/App/Main.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="" >
+        <m-header title="标题">
+            <div slot="left" @click="open = !open"><img src="../assets/stealth.png" /></div>
+        </m-header>
+        <m-aside :addClass="open">
+        	<div slot="butta" @click="open = !open">
+                关闭
+            </div>
+        </m-aside>
+    </div>
+</template>
+
+<script>
+import MHeader from '@/components/MHeader';
+import MAside from '@/components/MAside'
+
+export default {
+    name: 'Main',
+    data(){
+        return {
+            open:false
+        }
+    },
+    components: {
+        MHeader,
+        MAside
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/MAside.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="">
+	<transition name="fage">
+		<div class="zMouse-aside" v-if="addClass" @click.stop>
+			 <slot name="butta"></slot>
+		</div>
+	</transition>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'MAside',
+  props:['addClass']
+};
+</script>
+
+<style lang="css">
+@keyframes zMousetranslateX{
+	0%{
+		transform: translateX(0px)
+	}
+	100%{
+		transform: translateX(100%)
+	}
+}
+.zMouse-aside{
+	height: 100%;
+	position: absolute;
+	background:#ccc;
+	width: 65%;
+	left: 0;
+	top: 0;
+}
+
+.fage-enter-active{
+	animation: goin .5s linear;
+}
+.fage-leave-active{
+	animation: goout .5s linear;
+}
+@keyframes goin {
+	0%{
+		transform: translateX(-65%)
+	}
+	100%{
+		transform: translateX(0px)
+	}
+}
+@keyframes goout {
+	0%{
+		transform: translateX(0px)
+	}
+	100%{
+		transform: translateX(-100%)
+	}
+}
+
+</style>
